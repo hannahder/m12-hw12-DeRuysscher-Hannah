5f44ba834fa58b4d4026ec9e9f4662b6734b9154
Hannah De Ruysscher <hderuysscher@ufl.edu>
2021-03-30 16:00:29 -0400

    Added style

---
+++ main.js
@@ -0,0 +1,4 @@
+//get random value from array
+
+const colors = ["red","blue","green","yellow","pink","purple","black","white","orange","grey"];
+const randColor = colors[Math.floor(Math.random() * colors.length)];
